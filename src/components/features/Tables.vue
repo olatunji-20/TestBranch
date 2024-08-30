04f6a87557902c4673acb3567527acfe31e04789
falana sheriff <falanasheriffdeen@gmail.com>
2024-08-30 15:21:12 +0100

    completed features and pricing layout

--- src/components/features/Tables.vue
+++ src/components/features/Tables.vue
@@ -1,6 +1,65 @@
 <template>
-  <div class="border-2 border-blue-500 py-20">
-    FEATURES TABLE
+  <div class="border-2 border-blue-500 p-20">
+    <q-markup-table flat>
+      <thead>
+        <tr>
+          <th class="text-left">Features</th>
+          <th class="text-center">Free</th>
+          <th class="text-center">Starter</th>
+          <th class="text-center">Premium</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="bg-gray-200">
+          <td class="text-left">Branching Logic</td>
+          <td class="text-center"><q-icon name="sms" /></td>
+          <td class="text-center">6</td>
+          <td class="text-center">24</td>
+        </tr>
+        <tr>
+          <td class="text-left">Email Support</td>
+          <td class="text-center">237</td>
+          <td class="text-center">9</td>
+          <td class="text-center">37</td>
+        </tr>
+        <tr class="bg-gray-200">
+          <td class="text-left">Customizable Templates</td>
+          <td class="text-center">262</td>
+          <td class="text-center">16</td>
+          <td class="text-center">23</td>
+        </tr>
+        <tr>
+          <td class="text-left">Sub Accounts</td>
+          <td class="text-center">305</td>
+          <td class="text-center">3.7</td>
+          <td class="text-center">67</td>
+        </tr>
+        <tr class="bg-gray-200">
+          <td class="text-left">Send Certificates</td>
+          <td class="text-center">356</td>
+          <td class="text-center">16</td>
+          <td class="text-center">49</td>
+        </tr>
+        <tr>
+          <td class="text-left">Remote API Publishing</td>
+          <td class="text-center">356</td>
+          <td class="text-center">16</td>
+          <td class="text-center">49</td>
+        </tr>
+        <tr class="bg-gray-200">
+          <td class="text-left">Monthly Respondent Traffic Limit</td>
+          <td class="text-center">356</td>
+          <td class="text-center">16</td>
+          <td class="text-center">49</td>
+        </tr>
+        <tr class="">
+          <td class="text-left"></td>
+          <td class="text-center"><q-btn rounded outline class="w-[150px]" label="Get Started" /></td>
+          <td class="text-center"><q-btn rounded outline class="w-[150px]" label="Buy Plan" /></td>
+          <td class="text-center"><q-btn rounded outline class="w-[150px]" label="Buy Plan" /></td>
+        </tr>
+      </tbody>
+    </q-markup-table>
   </div>
 </template>
 
