38fe823cc3772be9dce19349df671c5b1ad1a557
falana sheriff <falanasheriffdeen@gmail.com>
2024-09-11 17:37:16 +0100

    progressed on the 'create new certificate' page

--- src/pages/certificate/AddCertificate.vue
+++ src/pages/certificate/AddCertificate.vue
@@ -1,19 +1,116 @@
 <template>
   <div class="border-2 border-green-400 p-8 bg-gray-100">
-    <div class="border-2 border-blue-400 p-4 rounded-md bg-white">
+    <div class="border border-blue-400 p-4 rounded-md bg-white">
         <router-link to="/certifications">
             <q-icon name="sms" class="bg-red-300 p-2 rounded-full q-mr-sm relative bottom-1" />
         </router-link>
         <h3 class="inline text-h5 font-bold">Create New Certificate</h3>
+        
+        <div class="flex flex-row items-center my-4">
+          <p class="text-secondary w-[auto] font-bold mr-2">Basic Details</p>
+          <hr class="flex-1 border border-purple-500" />
+        </div>
+        <div class="py-2 grid grid-cols-2 gap-4">
+          <div class="col-span-2">
+            <p class="font-bold">Certificate Name</p>
+            <q-input type="text" rounded outlined placeholder="Certification name" dense />
+          </div>
+          <div>
+            <p class="font-bold">Code (optional)</p>
+            <q-input type="text" rounded outlined placeholder="Code" dense />
+          </div>
+          <div>            
+            <p class="font-bold">Issuer</p>
+            <q-input type="text" rounded outlined placeholder="Issuer" dense />
+          </div>
+          <div>            
+            <p class="font-bold">Design</p>
+            <q-select :options="options" type="text" rounded outlined label="Select design" dense />
+          </div>
+          <div>            
+            <p class="font-bold">Group</p>
+            <q-select :options="options" type="text" rounded outlined label="Select group" dense />
+          </div>
+        </div>
+        
+        <div class="flex flex-row items-center my-4">
+          <p class="text-secondary w-[auto] font-bold mr-2">Start Date</p>
+          <hr class="flex-1 border border-purple-500" />
+        </div>
+        <div>
+          <q-radio keep-color v-model="cert" val="issued" label="Certificates are valid from the day they are issued" color="red" /><br />
+          <q-radio keep-color v-model="cert" val="fixed" label="Certificates are valid from a fixed date" color="red" />
+          <div v-if="cert === 'fixed'" class="p-2 w-[50%] ml-10 bg-gray-200">
+            <q-input class="rounded-full bg-white h-11 pt-1 px-4" size="sm" dense v-model="date" mask="date" :rules="['date']">
+              <template v-slot:append>
+                <q-icon name="event" class="cursor-pointer">
+                  <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                    <q-date v-model="date">
+                      <div class="row items-center justify-end">
+                        <q-btn v-close-popup label="Close" color="primary" flat />
+                      </div>
+                    </q-date>
+                  </q-popup-proxy>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </div>
+        
+        <div class="flex flex-row items-center my-4">
+          <p class="text-secondary w-[auto] font-bold mr-2">Expiry Date</p>
+          <hr class="flex-1 border border-purple-500" />
+        </div>
+        <div>
+          <q-checkbox keep-color v-model="expiry" val="issued" label="The certificate has an expiry date" color="red" /><br />
+
+          <div v-if="expiry" class="ml-1">
+            <q-radio keep-color v-model="expiryDate" val="fixedDuration" label="The certificate is valid for a fixed duration" color="red" /><br />
+            <div v-if="expiryDate === 'fixedDuration'" class="p-2 w-[50%] ml-10 bg-gray-200">
+              <q-select :options="options" rounded outlined label="1 Year" dense class="bg-white rounded-full" />            
+            </div>
+            <q-radio keep-color v-model="expiryDate" val="fixedDate" label="The certificate is valid until a fixed date" color="red" />
+            <div v-if="expiryDate === 'fixedDate'" class="p-2 w-[50%] ml-10 bg-gray-200">
+              <q-input class="rounded-full bg-white h-11 pt-1 px-4" size="sm" dense v-model="date" mask="date" :rules="['date']">
+                <template v-slot:append>
+                  <q-icon name="event" class="cursor-pointer">
+                    <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+                      <q-date v-model="date">
+                        <div class="row items-center justify-end">
+                          <q-btn v-close-popup label="Close" color="primary" flat />
+                        </div>
+                      </q-date>
+                    </q-popup-proxy>
+                  </q-icon>
+                </template>
+              </q-input>
+            </div>
+          </div>
+
+        </div>
+        
+        <div class="flex flex-row items-center my-4">
+          <p class="text-secondary w-[auto] font-bold mr-2">Certificate Sending</p>
+          <hr class="flex-1 border border-purple-500" />
+        </div>
+        
+        <div class="flex flex-row items-center my-4">
+          <p class="text-secondary w-[auto] font-bold mr-2">Additional Actions</p>
+          <hr class="flex-1 border border-purple-500" />
+        </div>
+        <p class="text-green-600 font-bold text-2xl">{{ expiry }}</p>
 
     </div>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import { ref } from 'vue';
+const options = ['option-1', 'option-2', 'option-3'];
+const cert = ref("")
+const expiry = ref(false)
+const expiryDate = ref("")
+const date = ref('2019/02/01')
 </script>
 
 <style>
--- src/pages/certificate/Certification.vue
+++ src/pages/certificate/Certification.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="border-2 border-red-500 h-[900px] p-8 bg-gray-200">
+    <div class="h-[100vh] p-8 bg-gray-200">
 
         <div class="">
             <q-table
@@ -14,6 +14,7 @@
                 :filter="filter"
                 binary-state-sort
                 @request="onRequest"
+                class="rounded-xl p-2"
             >
                 <template v-slot:top-right>
                     <q-input borderless dense debounce="300" v-model="filter" placeholder="Search for category" class="border border-red-400 rounded-full px-2">
@@ -23,7 +24,32 @@
                     </q-input>
                     <q-btn label="New Certificate" no-caps icon="sms" rounded class="q-ml-md q-px-lg" to="/add-certificate" />
                 </template>
-                
+
+                <template v-slot:body-cell-image="props">
+                    <q-td :props="props">
+                        <div class="w-[80px] h-[50px]">
+                            <img :src="props.row.image" :alt="props.row.protein" class="h-[100%] w-[100%]">
+                        </div>
+                    </q-td>
+                </template>
+
+                <template #body-cell-iron="props">
+                  <q-td :props="props">
+                    <q-btn-group rounded>
+                      <q-btn-dropdown auto-close no-caps rounded color="primary" label="Edit" split>
+                        <q-list padding style="width: 250px">
+                          <q-item clickable>
+                            <p>Copy</p>
+                          </q-item>
+                          <q-item clickable>
+                            <p>Delete</p>
+                          </q-item>
+                        </q-list>
+                      </q-btn-dropdown>
+                    </q-btn-group>
+                  </q-td>
+                </template>
+                 
             </q-table>
 
         </div>
@@ -35,65 +61,35 @@
   import { ref, onMounted } from 'vue'
   
   const columns = [
-    {
-      name: 'desc',
-      required: true,
-      label: 'Preview',
-      align: 'left',
-      field: row => row.name,
-      format: val => `${val}`,
-      sortable: true
-    },
-    { name: 'calories', align: 'center', label: 'Name', field: 'calories', sortable: true },
-    { name: 'fat', label: 'Group', field: 'fat', sortable: true },
-    // { name: 'carbs', label: 'Carbs (g)', field: 'carbs', sortable: true },
-    // { name: 'protein', label: 'Protein (g)', field: 'protein', sortable: true },
-    { name: 'sodium', label: 'Start date', field: 'sodium', sortable: true },
-    { name: 'calcium', label: 'Expiry date (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
-    { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+    { name: 'image', align: 'left', label: 'Preview', field: 'image', sortable: true },
+    { name: 'name', align: 'left', label: 'Name', field: 'name', sortable: true },
+    { name: 'fat', align: 'center', label: 'Group', field: 'fat', sortable: true },
+    { name: 'calcium', align: 'center', label: 'Start date', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+    { name: 'calcium', align: 'center', label: 'Expiry date', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+    { name: 'iron', align: 'center', label: '...', field: ''}
   ]
   
   const originalRows = [
-    { id: 1, name: 'Frozen Yogurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, sodium: 87, calcium: '14%', iron: '1%' },
-    { id: 2, name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, sodium: 129, calcium: '8%', iron: '1%' },
-    { id: 3, name: 'Eclair', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, sodium: 337, calcium: '6%', iron: '7%' },
-    { id: 4, name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, sodium: 413, calcium: '3%', iron: '8%' },
-    { id: 5, name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, sodium: 327, calcium: '7%', iron: '16%' },
-    { id: 6, name: 'Jelly bean', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, sodium: 50, calcium: '0%', iron: '0%' },
-    { id: 7, name: 'Lollipop', calories: 392, fat: 0.2, carbs: 98, protein: 0, sodium: 38, calcium: '0%', iron: '2%' },
-    { id: 8, name: 'Honeycomb', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, sodium: 562, calcium: '0%', iron: '45%' },
-    { id: 9, name: 'Donut', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, sodium: 326, calcium: '2%', iron: '22%' },
-    { id: 10, name: 'KitKat', calories: 518, fat: 26.0, carbs: 65, protein: 7, sodium: 54, calcium: '12%', iron: '6%' },
-    { id: 11, name: 'Frozen Yogurt-1', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, sodium: 87, calcium: '14%', iron: '1%' },
-    { id: 12, name: 'Ice cream sandwich-1', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, sodium: 129, calcium: '8%', iron: '1%' },
-    { id: 13, name: 'Eclair-1', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, sodium: 337, calcium: '6%', iron: '7%' },
-    { id: 14, name: 'Cupcake-1', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, sodium: 413, calcium: '3%', iron: '8%' },
-    { id: 15, name: 'Gingerbread-1', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, sodium: 327, calcium: '7%', iron: '16%' },
-    { id: 16, name: 'Jelly bean-1', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, sodium: 50, calcium: '0%', iron: '0%' },
-    { id: 17, name: 'Lollipop-1', calories: 392, fat: 0.2, carbs: 98, protein: 0, sodium: 38, calcium: '0%', iron: '2%' },
-    { id: 18, name: 'Honeycomb-1', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, sodium: 562, calcium: '0%', iron: '45%' },
-    { id: 19, name: 'Donut-1', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, sodium: 326, calcium: '2%', iron: '22%' },
-    { id: 20, name: 'KitKat-1', calories: 518, fat: 26.0, carbs: 65, protein: 7, sodium: 54, calcium: '12%', iron: '6%' },
-    { id: 21, name: 'Frozen Yogurt-2', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, sodium: 87, calcium: '14%', iron: '1%' },
-    { id: 22, name: 'Ice cream sandwich-2', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, sodium: 129, calcium: '8%', iron: '1%' },
-    { id: 23, name: 'Eclair-2', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, sodium: 337, calcium: '6%', iron: '7%' },
-    { id: 24, name: 'Cupcake-2', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, sodium: 413, calcium: '3%', iron: '8%' },
-    { id: 25, name: 'Gingerbread-2', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, sodium: 327, calcium: '7%', iron: '16%' },
-    { id: 26, name: 'Jelly bean-2', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, sodium: 50, calcium: '0%', iron: '0%' },
-    { id: 27, name: 'Lollipop-2', calories: 392, fat: 0.2, carbs: 98, protein: 0, sodium: 38, calcium: '0%', iron: '2%' },
-    { id: 28, name: 'Honeycomb-2', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, sodium: 562, calcium: '0%', iron: '45%' },
-    { id: 29, name: 'Donut-2', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, sodium: 326, calcium: '2%', iron: '22%' },
-    { id: 30, name: 'KitKat-2', calories: 518, fat: 26.0, carbs: 65, protein: 7, sodium: 54, calcium: '12%', iron: '6%' },
-    { id: 31, name: 'Frozen Yogurt-3', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, sodium: 87, calcium: '14%', iron: '1%' },
-    { id: 32, name: 'Ice cream sandwich-3', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, sodium: 129, calcium: '8%', iron: '1%' },
-    { id: 33, name: 'Eclair-3', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, sodium: 337, calcium: '6%', iron: '7%' },
-    { id: 34, name: 'Cupcake-3', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, sodium: 413, calcium: '3%', iron: '8%' },
-    { id: 35, name: 'Gingerbread-3', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, sodium: 327, calcium: '7%', iron: '16%' },
-    { id: 36, name: 'Jelly bean-3', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, sodium: 50, calcium: '0%', iron: '0%' },
-    { id: 37, name: 'Lollipop-3', calories: 392, fat: 0.2, carbs: 98, protein: 0, sodium: 38, calcium: '0%', iron: '2%' },
-    { id: 38, name: 'Honeycomb-3', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, sodium: 562, calcium: '0%', iron: '45%' },
-    { id: 39, name: 'Donut-3', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, sodium: 326, calcium: '2%', iron: '22%' },
-    { id: 40, name: 'KitKat-3', calories: 518, fat: 26.0, carbs: 65, protein: 7, sodium: 54, calcium: '12%', iron: '6%' }
+    { id: 1, name: 'Frozen Yogurt', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, image: 87, calcium: '14%', iron: '1%' },
+    { id: 2, name: 'Ice cream sandwich', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, image: 129, calcium: '8%', iron: '1%' },
+    { id: 3, name: 'Eclair', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, image: 337, calcium: '6%', iron: '7%' },
+    { id: 4, name: 'Cupcake', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, image: 'https://lh3.googleusercontent.com/ogw/AF2bZyijnjqVGapR6vLPqDFnf-UH8kC0amBcyvCgAr0BLrvsxp8=s32-c-mo', calcium: '3%', iron: '8%' },
+    { id: 5, name: 'Gingerbread', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, image: 327, calcium: '7%', iron: '16%' },
+    { id: 6, name: 'Jelly bean', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, image: 50, calcium: '0%', iron: '0%' },
+    { id: 7, name: 'Lollipop', calories: 392, fat: 0.2, carbs: 98, protein: 0, image: 38, calcium: '0%', iron: '2%' },
+    { id: 8, name: 'Honeycomb', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, image: 562, calcium: '0%', iron: '45%' },
+    { id: 9, name: 'Donut', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, image: 326, calcium: '2%', iron: '22%' },
+    { id: 10, name: 'KitKat', calories: 518, fat: 26.0, carbs: 65, protein: 7, image: 54, calcium: '12%', iron: '6%' },
+    { id: 11, name: 'Frozen Yogurt-1', calories: 159, fat: 6.0, carbs: 24, protein: 4.0, image: 87, calcium: '14%', iron: '1%' },
+    { id: 12, name: 'Ice cream sandwich-1', calories: 237, fat: 9.0, carbs: 37, protein: 4.3, image: 129, calcium: '8%', iron: '1%' },
+    { id: 13, name: 'Eclair-1', calories: 262, fat: 16.0, carbs: 23, protein: 6.0, image: 337, calcium: '6%', iron: '7%' },
+    { id: 14, name: 'Cupcake-1', calories: 305, fat: 3.7, carbs: 67, protein: 4.3, image: 413, calcium: '3%', iron: '8%' },
+    { id: 15, name: 'Gingerbread-1', calories: 356, fat: 16.0, carbs: 49, protein: 3.9, image: 327, calcium: '7%', iron: '16%' },
+    { id: 16, name: 'Jelly bean-1', calories: 375, fat: 0.0, carbs: 94, protein: 0.0, image: 50, calcium: '0%', iron: '0%' },
+    { id: 17, name: 'Lollipop-1', calories: 392, fat: 0.2, carbs: 98, protein: 0, image: 38, calcium: '0%', iron: '2%' },
+    { id: 18, name: 'Honeycomb-1', calories: 408, fat: 3.2, carbs: 87, protein: 6.5, image: 562, calcium: '0%', iron: '45%' },
+    { id: 19, name: 'Donut-1', calories: 452, fat: 25.0, carbs: 51, protein: 4.9, image: 'https://lh3.googleusercontent.com/ogw/AF2bZyijnjqVGapR6vLPqDFnf-UH8kC0amBcyvCgAr0BLrvsxp8=s32-c-mo', calcium: '2%', iron: '22%' },
+    { id: 20, name: 'KitKat-1', calories: 518, fat: 26.0, carbs: 65, protein: 7, image: 54, calcium: '12%', iron: '6%' }
   ]
   
   export default {
@@ -106,7 +102,7 @@
         sortBy: 'desc',
         descending: false,
         page: 1,
-        rowsPerPage: 5,
+        rowsPerPage: 3,
         rowsNumber: 10
       })
   
@@ -181,7 +177,7 @@
           loading.value = false
         }, 1500)
       }
-  
+
       onMounted(() => {
         // get initial data from server (1st page)
         tableRef.value.requestServerInteraction()
